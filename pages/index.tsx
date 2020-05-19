364213078d6e04955691bdd68d802a8632d1cc99
cima <rosa.cimalys@gmail.com>
2020-05-19 19:32:23 +0900

    Add a link to gacha-list on pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <main className="text-red-500">hellow, Randomness machine!!!!</main>
+  return (
+    <main className="text-red-500">
+      <div>hellow, Randomness machine!!!!</div>
+      <Link href="/gacha-list">
+        <a>Gacha List</a>
+      </Link>
+    </main>
+  )
 }
